9fa5b9ddeb047d8541120cc16ce12780de44a1ac
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-24 18:24:21 +0300

    🚀feat(Database) Connect to Database only once

--- core/Database.php
+++ core/Database.php
@@ -1,11 +1,37 @@
-<?php 
+<?php
 
 namespace Scandiweb;
 
 class Database
 {
-    public function __construct()
+
+    private static $connection = null;
+
+    /**
+     * Private constructor to disable instantiation from outside the class
+     */
+    private function __construct()
     {
-        echo 'Database loaded';
+        try {
+            static::$connection = new \PDO('mysql:host=localhost;dbname=scandiweb', 'root', 'toor', [
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ
+            ]);
+
+            echo 'Connected to DB <br>';
+        } catch (\PDOException $e) {
+            die('Failed connecting to db');
+        }
+    }
+
+    /**
+     * Always use single connection
+     */
+    public static function instance()
+    {
+        if (!static::$connection) {
+            static::$connection = new static;
+        }
+
+        return static::$connection;
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+use Scandiweb\Database;
+
 
 // Include the composer autoloader
 require __DIR__ . '/../vendor/autoload.php';
+
+
+Database::instance();
+Database::instance();
+Database::instance();
+Database::instance();
