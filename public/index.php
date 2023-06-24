a45a18e66cdafa39cff5fe10f2248841298ebc76
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-24 22:21:13 +0300

    ⚙️chore(index.php) define CONFIG_PATH and add comments

--- public/index.php
+++ public/index.php
@@ -7,5 +7,13 @@ use Scandiweb\Database;
 require __DIR__ . '/../vendor/autoload.php';
 
 
+define('CONFIG_PATH', __DIR__ . '/../config/');
+
+
+
+/**
+ * Should implement a service provider and put these inside
+ * But let's keep it simple for now
+ */
 // Add The Database instance to the container
 Container::set(Database::class, fn () => Database::instance());
