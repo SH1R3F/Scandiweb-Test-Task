af8fb9e0021c040939ac36bbb4ab5dabb5ee9fbe
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-25 19:34:14 +0300

    🛠️refactor(exceptions) handle validation and route not found exceptions; add default error view

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,15 +2,31 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use Scandiweb\Exceptions\RouteNotFound;
+use Scandiweb\Redirect;
+use Scandiweb\Session;
+use Scandiweb\Validation\Exceptions\ValidationException;
+use Scandiweb\View;
+use Throwable;
 
 class Handler
 {
 
-    public function __invoke(Exception $e)
+    public function __invoke(Throwable $e)
     {
-        die('Hello there');
+
+        if ($e instanceof ValidationException) {
+            // Redirect back with validation errors
+            Session::flash('errors', $e->errors());
+
+            Redirect::back();
+        }
+
+
+        if ($e instanceof RouteNotFound) {
+            http_response_code(404);
+        }
+
+        echo View::make('errors/default', ['message' => $e->getMessage()]);
     }
-
-
 }
---
+++ views/errors/default.php
@@ -0,0 +1,9 @@
+<?php include __DIR__ . '/../layout/header.php' ?>
+
+<div class="container my-5 py-5">
+    <div class="text-center">
+        <h1><?= $message ?></h1>
+    </div>
+</div>
+
+<?php include __DIR__ . '/../layout/footer.php' ?>
