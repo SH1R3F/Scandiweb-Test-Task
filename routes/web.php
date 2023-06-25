7a322c87be73c526711159dc5253cfc3af1be108
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-25 17:34:28 +0300

    🚀feat(product) add ability to create new product via form submission

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ use Scandiweb\Router;
 
 Router::get('/', [ProductController::class, 'index']);
 Router::get('/add-product', [ProductController::class, 'create']);
+Router::post('/add-product', [ProductController::class, 'store']);
 Router::delete('/products', [ProductController::class, 'destroy']);
