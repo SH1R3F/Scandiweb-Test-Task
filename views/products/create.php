5e54f70e799329d286e2913b1d6b850ecf249709
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-25 17:26:14 +0300

    🚀feat(product) create product form view page

---
+++ views/products/create.php
@@ -0,0 +1,133 @@
+<?php include __DIR__ . '/../layout/header.php' ?>
+
+<!-- Header -->
+<div class="header pt-5">
+    <div class="container">
+        <div class="row border-bottom">
+            <div class="col-sm-6">
+                <h1>Product Add</h1>
+            </div>
+            <div class="col-sm-6 text-end">
+                <button type="button" class="btn btn-light shadow-sm mx-1 border" class="btn">Save</button>
+                <a href="/" type="button" id="delete-product-btn" class="btn btn-light shadow-sm mx-1 border" class="btn">Cancel</a>
+            </div>
+        </div>
+    </div>
+</div><!-- Header -->
+
+<!-- Products List -->
+<div class="products pt-4" id="app">
+    <div class="container">
+
+        <!-- Delete Form -->
+        <form action="/#" method="POST" id="product_form">
+            <input type="hidden" name="csrf_token" value="TO BE ADDED">
+
+            <!-- SKU Field -->
+            <div class="mb-3 row">
+                <label for="sku" class="col-sm-2 col-form-label">SKU</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="sku">
+                </div>
+            </div><!-- SKU Field -->
+
+            <!-- Name Field -->
+            <div class="mb-3 row">
+                <label for="name" class="col-sm-2 col-form-label">Name</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="name">
+                </div>
+            </div><!-- Name Field -->
+
+            <!-- Price Field -->
+            <div class="mb-3 row">
+                <label for="price" class="col-sm-2 col-form-label">Price($)</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="price">
+                </div>
+            </div><!-- Price Field -->
+
+            <!-- Type Field -->
+            <div class="mb-3 row">
+                <label for="type" class="col-sm-2 col-form-label">Product type</label>
+                <div class="col-sm-10">
+                    <select name="type" class="form-select" aria-label="Product type" id="productType" v-model="type">
+                        <option value="book" id="Book">Book</option>
+                        <option value="dvd" id="DVD">DVD</option>
+                        <option value="furniture" id="Furniture">Furniture</option>
+                    </select>
+                </div>
+            </div><!-- Type Field -->
+
+
+
+            <div class="p-5">
+                <!-- Book Attributes -->
+                <div class="mb-3 row" v-if="type === 'book'">
+                    <small class="form-text">Please, provide weight</small>
+
+                    <label for="weight" class="col-sm-2 col-form-label">Weight (KG)</label>
+                    <div class="col-sm-10">
+                        <input name="attrs[weight]" type="text" class="form-control" id="weight">
+                    </div>
+                </div><!-- Book Attributes -->
+
+                <!-- DVD Attributes -->
+                <div class="mb-3 row" v-if="type === 'dvd'">
+                    <small class="form-text">Please, provide size</small>
+
+                    <label for="size" class="col-sm-2 col-form-label">Size (MB)</label>
+                    <div class="col-sm-10">
+                        <input name="attrs[size]" type="text" class="form-control" id="size">
+                    </div>
+                </div><!-- DVD Attributes -->
+
+                <!-- Furniture Attributes -->
+                <div class="furniture-attributes" v-if="type === 'furniture'">
+                    <small class="form-text">Please, provide dimensions</small>
+
+                    <div class="mb-3 row">
+                        <label for="height" class="col-sm-2 col-form-label">Height (CM)</label>
+                        <div class="col-sm-10">
+                            <input name="attrs[height]" type="text" class="form-control" id="height">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="width" class="col-sm-2 col-form-label">Width (CM)</label>
+                        <div class="col-sm-10">
+                            <input name="attrs[width]" type="text" class="form-control" id="width">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="length" class="col-sm-2 col-form-label">Length (CM)</label>
+                        <div class="col-sm-10">
+                            <input name="attrs[length]" type="text" class="form-control" id="length">
+                        </div>
+                    </div>
+                </div><!-- Furniture Attributes -->
+            </div>
+
+
+        </form><!-- Delete Form -->
+
+    </div>
+</div><!-- Products List -->
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script>
+    const {
+        createApp,
+        ref
+    } = Vue
+
+    createApp({
+        setup() {
+            const type = ref('book')
+            return {
+                type
+            }
+        }
+    }).mount('#app')
+</script>
+
+<?php include __DIR__ . '/../layout/footer.php' ?>
