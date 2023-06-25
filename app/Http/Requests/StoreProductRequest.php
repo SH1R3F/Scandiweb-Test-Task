9d070255df0b10dd77e2115a1c3243d769b44adb
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-25 17:46:01 +0300

    🛠️refactor(product): update validation rules for product attributes

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -9,15 +9,15 @@ class StoreProductRequest extends Request
     public function rules(): array
     {
         return [
-            'sku'    => ['required', 'unique:products,sku'],
-            'name'   => ['required'],
-            'price'  => ['required'],
-            'type'   => ['required'],
-            // 'weight' => ['required_if:type,book'],
-            // 'size'   => ['required_if:type,dvd'],
-            // 'height' => ['required_if:type,furniture'],
-            // 'width'  => ['required_if:type,furniture'],
-            // 'length' => ['required_if:type,furniture'],
+            'sku'          => ['required', 'unique:products,sku'],
+            'name'         => ['required'],
+            'price'        => ['required'],
+            'type'         => ['required'],
+            'attrs.weight' => ['required_if:type,book'],
+            'attrs.size'   => ['required_if:type,dvd'],
+            'attrs.height' => ['required_if:type,furniture'],
+            'attrs.width'  => ['required_if:type,furniture'],
+            'attrs.length' => ['required_if:type,furniture'],
         ];
     }
 }
