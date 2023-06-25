12708a23552861dfdcfd6185665756b3a1c2da47
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-25 16:35:18 +0300

    ⚙️chore(database) add ExistsRule for validation

---
+++ core/Validation/Rules/ExistsRule.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace Scandiweb\Validation\Rules;
+
+use Scandiweb\Container;
+use Scandiweb\Database;
+
+class ExistsRule implements Rule
+{
+
+    private ?string $error = null;
+    private ?Database $db = null;
+
+    public function __construct()
+    {
+        $this->db = Container::get(Database::class);
+    }
+
+
+    public function validate(string $key, mixed $value, ...$arguments): bool
+    {
+        [$table, $column] = $arguments;
+        $passed = !!$this->db->count($table, [$column => $value]);
+
+        if (!$passed) {
+            $this->error = "$key field doesn't exist";
+        }
+
+        return $passed;
+    }
+
+
+    public function error(): string
+    {
+        return $this->error;
+    }
+}
