bb717055bbb811fe0ac9a498cf0d2d0df262a83e
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-25 16:56:14 +0300

    ⚙️chore(view): extract header and footer into separate layout

---
+++ views/layout/footer.php
@@ -0,0 +1,12 @@
+    <!-- Footer -->
+    <div class="footer">
+        <div class="container border-top mt-3 py-3 text-center">
+            <span>Scandiweb Test assignment</span>
+        </div>
+    </div>
+    <!-- Footer -->
+
+    <script src="/assets/bootstrap.js"></script>
+</body>
+
+</html>
--- views/products/index.php
+++ views/products/index.php
@@ -1,73 +1,51 @@
-<!doctype html>
-<html lang="en">
+<?php include __DIR__ . '/../layout/header.php' ?>
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Product List</title>
-  <link href="/assets/bootstrap.css" rel="stylesheet">
-</head>
-
-<body>
-
-  <!-- Header -->
-  <div class="header pt-5">
-    <div class="container">
-      <div class="row border-bottom">
-        <div class="col-sm-6">
-          <h1>Product List</h1>
-        </div>
-        <div class="col-sm-6 text-end">
-          <a href="/add-product" type="button" class="btn btn-light shadow-sm mx-1 border" class="btn">ADD</a>
-          <button type="button" id="delete-product-btn" class="btn btn-light shadow-sm mx-1 border" class="btn" onclick="document.getElementById('deleteForm').submit()">MASS DELETE</button>
-        </div>
+<!-- Header -->
+<div class="header pt-5">
+  <div class="container">
+    <div class="row border-bottom">
+      <div class="col-sm-6">
+        <h1>Product List</h1>
+      </div>
+      <div class="col-sm-6 text-end">
+        <a href="/add-product" type="button" class="btn btn-light shadow-sm mx-1 border" class="btn">ADD</a>
+        <button type="button" id="delete-product-btn" class="btn btn-light shadow-sm mx-1 border" class="btn" onclick="document.getElementById('deleteForm').submit()">MASS DELETE</button>
       </div>
     </div>
-  </div><!-- Header -->
-
-  <!-- Products List -->
-  <div class="products pt-4">
-    <div class="container">
-
-      <!-- Delete Form -->
-      <form action="/products" method="POST" id="deleteForm">
-        <input type="hidden" name="_method" value="DELETE">
-        <input type="hidden" name="csrf_token" value="TO BE ADDED">
-        <div class="row">
-
-          <?php foreach ($products as $product) : ?>
-            <!-- Single Product -->
-            <div class="col-md-3 col-sm-4">
-              <div class="card shadow border mb-3">
-                <div class="card-body text-secondary">
-                  <input class="form-check-input delete-checkbox" type="checkbox" name="products[]" value="<?= $product->id ?>">
-                  <p class="card-text text-center">
-                    <?= $product->sku ?><br>
-                    <?= $product->name ?><br>
-                    <?= $product->price ?>$<br>
-                    <?= $product->attrs ?><br>
-                  </p>
-                </div>
-              </div>
-            </div><!-- Single Product -->
-          <?php endforeach; ?>
-
-        </div>
-      </form><!-- Delete Form -->
-
-    </div>
-  </div><!-- Products List -->
-
-
-  <!-- Footer -->
-  <div class="footer">
-    <div class="container mt-3 py-3 border-top text-center">
-      <span>Scandiweb Test assignment</span>
-    </div>
   </div>
-  <!-- Footer -->
+</div><!-- Header -->
 
-  <script src="/assets/bootstrap.js"></script>
-</body>
+<!-- Products List -->
+<div class="products pt-4">
+  <div class="container">
 
-</html>
+    <!-- Delete Form -->
+    <form action="/products" method="POST" id="deleteForm">
+      <input type="hidden" name="_method" value="DELETE">
+      <input type="hidden" name="csrf_token" value="TO BE ADDED">
+      <div class="row">
+
+        <?php foreach ($products as $product) : ?>
+          <!-- Single Product -->
+          <div class="col-md-3 col-sm-4">
+            <div class="card shadow border mb-3">
+              <div class="card-body text-secondary">
+                <input class="form-check-input delete-checkbox" type="checkbox" name="products[]" value="<?= $product->id ?>">
+                <p class="card-text text-center">
+                  <?= $product->sku ?><br>
+                  <?= $product->name ?><br>
+                  <?= $product->price ?>$<br>
+                  <?= $product->attrs ?><br>
+                </p>
+              </div>
+            </div>
+          </div><!-- Single Product -->
+        <?php endforeach; ?>
+
+      </div>
+    </form><!-- Delete Form -->
+
+  </div>
+</div><!-- Products List -->
+
+<?php include __DIR__ . '/../layout/footer.php' ?>
